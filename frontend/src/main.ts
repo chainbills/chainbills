947beb1e5dc92e04298a78072c1ffafdb1f7b4e2
Obum (@obumnwabude) <obumnwabude@gmail.com>
2024-03-29 22:25:01 +0100

    feat: build landing page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,12 @@
+import 'aos/dist/aos.css';
 import 'primevue/resources/themes/aura-light-green/theme.css';
 import './assets/main.css';
 
+import AOS from 'aos';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
+import VueWriter from 'vue-writer';
 
 import App from './App.vue';
 import router from './router';
@@ -13,5 +16,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(PrimeVue, { ripple: true });
 app.use(router);
+app.use(VueWriter);
 
 app.mount('#app');
+
+AOS.init({ duration: 1200 });
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,1 +1,118 @@
-<template></template>
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
+<template>
+  <section class="pt-12 pb-20">
+    <h2 class="text-center font-bold text-blue-500 text-3xl sm:text-7xl">
+      <vue-writer
+        :array="['Pay Merchants', 'Make Donations', 'Subscribe to Creators']"
+      />
+    </h2>
+  </section>
+
+  <section class="py-20">
+    <h2
+      class="text-2xl sm:text-5xl mb-8 text-center font-black flex flex-col sm:leading-10"
+    >
+      <span class="mb-2 sm:mb-4">Receive </span>
+      <span class="mb-2 sm:mb-4 blue-gradient">any crypto</span>
+      <span class="mb-2 sm:mb-4"
+        >on <span class="blue-gradient">any chain</span></span
+      >
+      <span class="mb-2 sm:mb-4"
+        >from <span class="blue-gradient">any one</span></span
+      >
+    </h2>
+    <p class="text-center">
+      <Button class="bg-blue-500 text-white dark:text-black text-xl px-6 py-2"
+        >Get Started</Button
+      >
+    </p>
+  </section>
+
+  <section class="py-20">
+    <h2 class="text-5xl text-center font-bold mb-8" data-aos="fade-up">
+      How it Works
+    </h2>
+    <ol class="text-2xl max-w-96 mx-auto mb-8">
+      <li class="mb-4" data-aos="fade-up">1. Create a Payable (public invoice)</li>
+      <li class="mb-4" data-aos="fade-up">2. Share the link</li>
+      <li class="mb-4" data-aos="fade-up">3. Get Paid</li>
+      <li class="mb-4" data-aos="fade-up">4. Withdraw at anytime</li>
+    </ol>
+    <p class="text-center">
+      <Button
+        class="bg-blue-500 text-white dark:text-black text-xl px-6 py-2"
+        data-aos="fade-up"
+        >Get Started</Button
+      >
+    </p>
+  </section>
+
+  <section class="py-20">
+    <h2 class="text-5xl text-center font-bold mb-8" data-aos="fade-up">
+      Withdraw Now
+    </h2>
+    <p class="text-2xl max-w-96 mx-auto mb-4 text-center" data-aos="fade-up">
+      View your balances and withdraw at anytime.
+    </p>
+    <p class="text-gray-500 text-center mb-12" data-aos="fade-up">
+      * We charge 2% on every withdrawal
+    </p>
+    <p class="text-center">
+      <Button
+        class="bg-blue-500 text-white dark:text-black text-xl px-6 py-2"
+        data-aos="fade-up"
+        >Dashboard</Button
+      >
+    </p>
+  </section>
+
+  <section class="py-20">
+    <h2 class="text-5xl text-center font-bold mb-8" data-aos="fade-up">
+      Keeping Records
+    </h2>
+    <p class="text-2xl max-w-96 mx-auto mb-12 text-center" data-aos="fade-up">
+      Keep track of all your payments and withdrawals on
+      <span class="font-bold text-blue-500">Chainbills</span>.
+    </p>
+    <p class="text-center">
+      <Button
+        class="bg-blue-500 text-white dark:text-black text-xl px-6 py-2"
+        data-aos="fade-up"
+        >My Activity</Button
+      >
+    </p>
+  </section>
+  <section class="pt-20 pb-32 md:flex justify-between max-w-screen-md mx-auto">
+    <h2 class="text-4xl font-bold mb-8 basis-3/5" data-aos="fade-up">
+      Start Receiving Payments Today!
+    </h2>
+    <p>
+      <Button
+        class="bg-blue-500 text-white dark:text-black text-xl px-6 py-2"
+        data-aos="fade-up"
+        >Get Started</Button
+      >
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.blue-gradient {
+  background: -webkit-linear-gradient(left, var(--blue-100), var(--blue-900));
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+body.dark .blue-gradient {
+  background: -webkit-linear-gradient(
+    left,
+    var(--blue-800) 40%,
+    var(--blue-300)
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>

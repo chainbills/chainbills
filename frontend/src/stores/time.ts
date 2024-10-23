ab65cc80600dc76e437190fe73b7a81b17865507
Obum (@obumnwabude) <obumnwabude@gmail.com>
2024-10-23 12:38:36 +0100

    chore: improve the interval update of time to save browser resources

--- frontend/src/stores/time.ts
+++ frontend/src/stores/time.ts
@@ -49,13 +49,34 @@ export const useTimeStore = defineStore('time', () => {
       new Date(new Date().setHours(0, 0, 0, 0)).getTime() / 1000
     );
     const lastTwoMidnights = lastMidnight - 24 * 60 * 60;
+
+    // If the time is within the last two midnights, update the displayed value
+    // 
+    // Not updating the value otherwise because the displayed value will
+    // become an absolute date and no longer a relative one
     if (lastTwoMidnights < when) {
       // get the number of seconds till the next minute from now
       const seconds = 60 - (Math.round(Date.now() / 1000) % 60);
-      // update the displayed value after the next minute for every minute
+
+      // update the displayed value after the next minute and set 
+      // a listener by then.
       setTimeout(() => {
         displayed.value = getDisplay(when);
-        setInterval(() => (displayed.value = getDisplay(when)), 60 * 1000);
+
+        // Compute the interval for updating the displayed value
+        // Update by the minute if the time is within the last hour
+        // Otherwise, update by the hour
+        let interval;
+        const now = Math.round(Date.now() / 1000);
+        const date = new Date(when * 1000);
+        if (now - 60 * 60 < when && new Date().getHours() === date.getHours()) {
+          interval = 60 * 1000;
+        } else {
+          interval = 60 * 60 * 1000;
+        }
+        
+        // Update the displayed value every interval
+        setInterval(() => (displayed.value = getDisplay(when)), interval);
       }, seconds);
     }
 

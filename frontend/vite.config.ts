cb767b6b6ddf15b6ea3bcd30a540f9bea127e9f5
Emontiga <183389730+emontiga@users.noreply.github.com>
2025-04-25 22:48:19 +0100

    chore: visualize dependency sizes after build with rollup plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), nodePolyfills()],
+  plugins: [vue(), vueDevTools(), nodePolyfills(), visualizer()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
